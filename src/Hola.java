cb0030547222510795e810c26b1bae6489305edc
Marta <mgomezdeleon@ceinmark.net>
2023-04-11 19:41:21 +0200

    Modificado por Marta

--- src/Hola.java
+++ src/Hola.java
@@ -3,6 +3,8 @@ public class Hola {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Ceinmark");
+		System.out.println("Hola, soy Marta");
+		System.out.println("Es Martes");
 	}
 
 }
